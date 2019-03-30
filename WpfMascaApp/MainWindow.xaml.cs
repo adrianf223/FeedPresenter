cef64d328422bb708059fd4261fc30fb8ffcedaf
Adrian <adrianf223@gmail.com>
2019-03-31 01:22:01 +0200

    added html to plain text for summary, renaming

--- WpfMascaApp/MainWindow.xaml.cs
+++ WpfMascaApp/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net;
 using System.IO;
 using System.ServiceModel.Syndication;
 using System.Xml;
-using System.Globalization;
-using System.Windows.Markup;
+using System.Text.RegularExpressions;
 
 namespace WpfMascaApp
 {
@@ -26,7 +16,7 @@ namespace WpfMascaApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        string theurl = "https://www.stiridecluj.ro/rss.xml";
+        string theurl = "https://entwickler.de/windowsdeveloper/feed/rss2";
         SyndicationFeed downloadFeed;
 
         public MainWindow()
@@ -42,7 +32,7 @@ namespace WpfMascaApp
                 webClientInstance.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
                 var result = webClientInstance.DownloadData(new Uri(theurl));
-                UTF8Encoding utf8 = new UTF8Encoding();
+                var utf8 = new UTF8Encoding();
                 var webdata = utf8.GetString(result);
 
                 using (StringReader stringReaderInstance = new StringReader(webdata.ToString()))
@@ -53,15 +43,51 @@ namespace WpfMascaApp
 
                 var firstPost = downloadFeed.Items.FirstOrDefault();
 
-                var imgurl = firstPost.Links.FirstOrDefault(link => link.RelationshipType.Contains("enclosure")).Uri;
+                var imgurl = firstPost.Links.FirstOrDefault(link => link.RelationshipType.Contains("enclosure"))?.Uri;
 
-                imgbackground.Source = new BitmapImage(imgurl);
-                imagethumb.Source = new BitmapImage(imgurl);
+                if (imgurl != null)
+                {
+                    imgbackground.Source = new BitmapImage(imgurl);
+                    imagethumb.Source = new BitmapImage(imgurl);
+                }
 
-                feedTitle.Content = firstPost.Title.Text;                    
-                feedSummary.Text =  firstPost.Summary.Text;
+                feedTitle.Text = firstPost.Title.Text;
+
+                //var webtext =  firstPost.Summary.Text;
+
+                //var writer = new StringWriter();
+                //WebUtility.HtmlEncode(webtext, writer);
+
+                //feedSummary.Text =  writer.ToString(); 
+                //feedSummary.Selection.Text = WebUtility.HtmlEncode(firstPost.Summary.Text);
+                feedSummary.Text = HtmlToPlainText( firstPost.Summary.Text );
             }
 
+        }
+
+        // from: 
+        // https://stackoverflow.com/questions/286813/how-do-you-convert-html-to-plain-text
+        //
+        private static string HtmlToPlainText(string html)
+        {
+            const string tagWhiteSpace = @"(>|$)(\W|\n|\r)+<";//matches one or more (white space or line breaks) between '>' and '<'
+            const string stripFormatting = @"<[^>]*(>|$)";//match any character between '<' and '>', even when end tag is missing
+            const string lineBreak = @"<(br|BR)\s{0,1}\/{0,1}>";//matches: <br>,<br/>,<br />,<BR>,<BR/>,<BR />
+            var lineBreakRegex = new Regex(lineBreak, RegexOptions.Multiline);
+            var stripFormattingRegex = new Regex(stripFormatting, RegexOptions.Multiline);
+            var tagWhiteSpaceRegex = new Regex(tagWhiteSpace, RegexOptions.Multiline);
+
+            var text = html;
+            //Decode html specific characters
+            text = System.Net.WebUtility.HtmlDecode(text);
+            //Remove tag whitespace/line breaks
+            text = tagWhiteSpaceRegex.Replace(text, "><");
+            //Replace <br /> with line breaks
+            text = lineBreakRegex.Replace(text, Environment.NewLine);
+            //Strip formatting
+            text = stripFormattingRegex.Replace(text, string.Empty);
+
+            return text;
         }
     }
 }
